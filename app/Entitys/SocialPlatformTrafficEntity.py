8838aabf3bf40475f85373de999e4cbe72ecac13
zs <1214391613@qq.com>
2024-10-12 14:42:13 +0800

    添加了类构造器

--- app/Entitys/SocialPlatformTrafficEntity.py
+++ app/Entitys/SocialPlatformTrafficEntity.py
@@ -11,3 +11,16 @@ class SocialPlatformTrafficEntity(BaseEntity):
     rate = db.Column(db.Float, nullable=False)  # 占比
     change = db.Column(db.Float, nullable=False)  # 变化率
     year_months = db.Column(db.BigInteger, nullable=False)  # 几年几月 2023-01
+
+    def __init__(
+        self, web_id=None, name=None, rate=None, change=None, year_months=None, **kwargs
+    ):
+        self.web_id = web_id
+        self.name = name
+        self.rate = rate
+        self.change = change
+        self.year_months = year_months
+        super().__init__(**kwargs)
+
+    def __repr__(self):
+        return f"<SocialPlatformTrafficEntity {self.id}>"
